6fec10455d8390a155dedc941a74ae7d969c6eac
Tanisha Singh <singhtanisha21@gmail.com>
2019-11-10 17:04:45 +0530

    yp

--- includes/header.php
+++ includes/header.php
@@ -17,11 +17,7 @@
     <!-- Custom CSS -->
       <script src="https://kit.fontawesome.com/2f58c8ad2d.js" crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
--- includes/navigation.php
+++ includes/navigation.php
@@ -1,12 +1,19 @@
 <?php session_start();
 ?>
+
  <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
       <a class="navbar-brand" href="#">Smartphone Shopping</a>
     </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="shop.php">Home</a></li>
+      <li><a href="shop.php">Home</a></li>
       
     </ul>
     <ul class="nav navbar-nav navbar-right">
@@ -21,7 +28,8 @@ echo "  <li><a href='includes/logout.php'><span class='glyphicon glyphicon-log-i
 
 		}
 		?>
-    
     </ul>
   </div>
+  </div>
 </nav>
+
